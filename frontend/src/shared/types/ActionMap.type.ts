c8e378e60ea27f6186c18b143ddb3417c81416ce
idbeholdv18 <alimsadullaev18@gmail.com>
2025-03-03 22:11:42 +0300

    ActionMap type for stateful ui-kit components

--- frontend/src/shared/types/ActionMap.type.ts
+++ frontend/src/shared/types/ActionMap.type.ts
@@ -1,4 +1,4 @@
-type ActionMap<M extends { [index: string]: any }> = {
+export type ActionMap<M extends { [index: string]: any }> = {
   [Key in keyof M]: M[Key] extends undefined
     ? {
         type: Key;
